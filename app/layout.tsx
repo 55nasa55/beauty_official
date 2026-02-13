359ef3c406d2aa795089cb78ff2ae15b72531aa8
55nasa55 <cjohnny533@gmail.com>
2026-02-12 18:50:17 -0800

    Updated layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { CartProvider } from '@/lib/cart-context';
 import { AddedToCartModal } from '@/components/cart/AddedToCartModal';
 import { Toaster } from '@/components/ui/toaster';
 import Providers from './providers';
+import { MarketingModal } from '@/components/MarketingModal';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Providers>
           <CartProvider>
+            <MarketingModal />
             {children}
             <AddedToCartModal />
             <Toaster />
