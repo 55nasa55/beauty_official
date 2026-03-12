0d0a98ecdc8211ba2dc46ea36d8cf925630232fa
55nasa55 <cjohnny533@gmail.com>
2026-03-11 18:32:40 -0700

    Updated index.ts

--- supabase/functions/order-sync/index.ts
+++ supabase/functions/order-sync/index.ts
@@ -7,7 +7,7 @@ const corsHeaders = {
   "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Client-Info, Apikey",
 };
 
-// TEST 5
+// TEST 6
 interface Order {
   id: string;
   order_number: string;
